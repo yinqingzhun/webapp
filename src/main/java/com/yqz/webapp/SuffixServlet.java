d6545d5fea85bc2b992a59fc26ede696a2594a96
yinqingzhun <yinqingzhun@autohome.com.cn>
2016-12-07 17:36:33 +0800

    spring mvc

--- src/main/java/com/yqz/webapp/SuffixServlet.java
+++ src/main/java/com/yqz/webapp/SuffixServlet.java
@@ -4,10 +4,10 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class SimpleServlet2 extends HttpServlet {
+public class SuffixServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("SimpleServlet2 Executed");
+		out.println("SuffixServlet Executed");
 		out.flush();
 		out.close();
 	}
