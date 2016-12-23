92bf7345257745dddc61b88dbcef408563e37ed2
yinqingzhun <yinqingzhun@autohome.com.cn>
2016-12-23 18:45:44 +0800

    编程配置

--- src/main/java/com/yqz/webapp/config/WebConfig.java
+++ src/main/java/com/yqz/webapp/config/WebConfig.java
@@ -3,11 +3,13 @@ package com.yqz.webapp.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan
-public class WebConfig {
-
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	
 	
 }
