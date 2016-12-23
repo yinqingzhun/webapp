9518bccc202efeec84c563164b8337dcb0ce4095
yinqingzhun <yinqingzhun@autohome.com.cn>
2016-12-23 19:19:36 +0800

    config

--- src/main/java/com/yqz/webapp/config/RootConfig.java
+++ src/main/java/com/yqz/webapp/config/RootConfig.java
@@ -1,16 +1,15 @@
 package com.yqz.webapp.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-/*@EnableWebMvc
 @Configuration
-@ComponentScan(basePackageClasses = SpringConfig.class)*/
-public class SpringConfig extends WebMvcConfigurationSupport {
+@ComponentScan(excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig extends WebMvcConfigurationSupport {
 
-	public SpringConfig(){
-		
-	}
+	 
 }
--- src/main/java/com/yqz/webapp/config/WebConfig.java
+++ src/main/java/com/yqz/webapp/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.yqz.webapp.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
@@ -11,5 +14,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver vr=new InternalResourceViewResolver();
+		vr.setPrefix("/web-inf/views");
+		vr.setSuffix("jsp");
+		vr.setExposeContextBeansAsAttributes(true);
+		vr.setExposePathVariables(true);
+		return vr;
+	}
 	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		 configurer.enable();
+	}
 }
